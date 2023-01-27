2852987465bf70f66824b8aa777559d05f7f6abd
nweinstock9 <nweinstock@uchicago.edu>
2023-01-27 19:31:32 +0000

    square new done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-
+  def square_the_number
+    render({ :template => "formtemplates/user_square_number.html.erb"})
+  end
+  def square_results
+    @input = params.fetch("number")
+    @result = @input.to_f**2
+    render({ :template => "formtemplates/user_square_results.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-
+  get("/square/results", { :controller => "application", :action => "square_results"})
+  get("/square/new", { :controller => "application", :action => "square_the_number"})
 end
