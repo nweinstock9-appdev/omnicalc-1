36e79256d93cd62d2c3b6d348655c950ef76eba6
nweinstock9 <nweinstock@uchicago.edu>
2023-01-27 19:48:01 +0000

    square root form works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
     @result = @input.to_f**2
     render({ :template => "formtemplates/user_square_results.html.erb"})
   end
+  def user_sqrt_input
+    render({ :template => "formtemplates/user_sqrt.html.erb"})
+  end
+  def user_sqrt_results
+    @sqrt_input = params.fetch("sqrt_number")
+    @sqrt_result = @sqrt_input.to_f**0.5
+    render({ :template => "formtemplates/user_sqrt_results.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get("/square/results", { :controller => "application", :action => "square_results"})
   get("/square/new", { :controller => "application", :action => "square_the_number"})
+  get("/square_root/results", { :controller => "application", :action => "user_sqrt_results"})
+  get("/square_root/new", { :controller => "application", :action => "user_sqrt_input"})
 end
