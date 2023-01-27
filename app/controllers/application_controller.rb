b8b168f05b17036705b572786eba8a40bffd4143
nweinstock9 <nweinstock@uchicago.edu>
2023-01-27 21:05:35 +0000

    Payment Form Works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,21 @@ class ApplicationController < ActionController::Base
     @sqrt_result = @sqrt_input.to_f**0.5
     render({ :template => "formtemplates/user_sqrt_results.html.erb"})
   end
+  def user_payment_results
+    apr_percentage = (params.fetch("apr_input").to_f)/100
+    n = (params.fetch("years_input").to_f)*12
+    r = apr_percentage/12
+    principal = (params.fetch("principal_input")).to_f
+    num = (r*principal)
+    denom = 1-((1+r)**(-1*n))
+    payment = (num/denom)
+    @apr_output = (params.fetch("apr_input")).to_f.to_s(:percentage, { :precision => 4})
+    @n_output = (n/12).to_s
+    @principal_output = principal.to_s(:currency)
+    @payment_results = payment.to_s(:currency)
+    render({ :template => "formtemplates/user_payment_results.html.erb"})
+  end
+  def user_payment_new
+    render({ :template => "formtemplates/user_payment_new.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get("/square/new", { :controller => "application", :action => "square_the_number"})
   get("/square_root/results", { :controller => "application", :action => "user_sqrt_results"})
   get("/square_root/new", { :controller => "application", :action => "user_sqrt_input"})
+  get("/payment/results", { :controller => "application", :action => "user_payment_results"})
+  get("/payment/new", { :controller => "application", :action => "user_payment_new"})
 end
